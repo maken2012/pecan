9fd035f5efeb77eb721c92e824b6bfbe9a8b9b12
maken <2544920363@qq.com>
2020-03-06 16:08:55 +0800

    build project

---
+++ src/main/java/org/nuts/pecan/PecanApplication.java
@@ -0,0 +1,13 @@
+package org.nuts.pecan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PecanApplication.class, args);
+    }
+
+}
